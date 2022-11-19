06ea95533c5976ebbabce242de8de830c8bc8535
panik5467 <53363577+panik5467@users.noreply.github.com>
2022-11-19 13:00:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-app.get("/", (req, res) => {
-  res.send(200, '<H1>Hello, nodejs server example</H1>');
-});
+//app.get("/", (req, res) => {
+//  res.send(200, '<H1>Hello, nodejs server example</H1>');
+//});
 
 app.get("/users", (req, res) => {
   res.send(200, db);
