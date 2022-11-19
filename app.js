546995c80ff8928f525805378621a60ff90e6d38
panik5467 <53363577+panik5467@users.noreply.github.com>
2022-11-19 12:51:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send(200, 'Hello, nodejs server example');
+});
+
 app.get("/users", (req, res) => {
   res.send(200, db);
 });
