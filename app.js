e0d1175ec433756ce8e51ded1b06edcc6976f2b4
panik5467 <53363577+panik5467@users.noreply.github.com>
2022-11-20 13:04:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const app = require("express")();
+const express = require('express');
 const db = require("./db.json");
 const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
